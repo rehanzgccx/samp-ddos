0d2e43ede46161d260a55112fb339daa16a9b82f
Nicholas Developer <93073492+NicholasDevelopers@users.noreply.github.com>
2022-12-07 21:04:52 +0700

    Update nicholas.py

--- nicholas.py
+++ nicholas.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
          os.system('cls' if os.name == 'nt' else 'clear')
          
          print('#########################################################################')
-         print('DiegoBot')
+         print('Bacot Lo Ngentot')
          print('#########################################################################')
          print('\n\n')
          print('{}'.format(orgip))
